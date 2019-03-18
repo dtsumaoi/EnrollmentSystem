d2bbf16312230da28f3833ab0069b76a7c89465e
dtsumaoi <dtsumaoi@gmail.com>
2019-03-18 18:15:17 +0800

    initialize class

--- EnrollmentSystem/src/enrollmentsystem/EnrollmentSystem.java
+++ EnrollmentSystem/src/enrollmentsystem/EnrollmentSystem.java
@@ -11,11 +11,9 @@ package enrollmentsystem;
  */
 public class EnrollmentSystem {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
     }
-    
 }
+
+
